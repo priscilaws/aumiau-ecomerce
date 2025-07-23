7da42741b7b25b0975bf9ddc29adee1f88d15c1f
Priscila <pri.wagner.silva@gmail.com>
2025-07-23 16:27:31 +0100

    added product card

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,6 +1,62 @@
+import React from "react";
 import Banner from "../../components/Banner";
 import bannerImage from "../../assets/images/three-dogs-banner.png";
+import ProductCard from "../../components/Products";
+import ImgProduro from "../../assets/images/petmarket.png";
 
 export default function Produtos() {
-  return <Banner image={bannerImage} alt="3 cachorros" />;
+  return (
+    <>
+      <Banner image={bannerImage} alt="3 cachorros" />
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+          gap: "16px",
+          marginTop: "24px",
+        }}
+      >
+        <ProductCard
+          image={ImgProduro}
+          title="Titulo do Produto"
+          description="descrição do produto aumiau"
+        />
+        <ProductCard
+          image={ImgProduro}
+          title="Outro Produto"
+          description="Outra descrição"
+        />
+        <ProductCard
+          image={ImgProduro}
+          title="Outro Produto"
+          description="Outra descrição"
+        />
+        <ProductCard
+          image={ImgProduro}
+          title="Outro Produto"
+          description="Outra descrição"
+        />
+        <ProductCard
+          image={ImgProduro}
+          title="Outro Produto"
+          description="Outra descrição"
+        />
+        <ProductCard
+          image={ImgProduro}
+          title="Outro Produto"
+          description="Outra descrição"
+        />
+        <ProductCard
+          image={ImgProduro}
+          title="Outro Produto"
+          description="Outra descrição"
+        />
+        <ProductCard
+          image={ImgProduro}
+          title="Outro Produto"
+          description="Outra descrição"
+        />
+      </div>
+    </>
+  );
 }
